6719f6e9956c7eeef4f759d6ac0fde80cc2249ba
yvonnegichovi <yvonnegichovi@gmail.com>
2025-07-15 00:41:53 +0300

    fix

--- alx_travel_app/alx_travel_app/alx_travel_app/settings.py
+++ alx_travel_app/alx_travel_app/alx_travel_app/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-z5lesxl4=#g#_k*ax#tmxzll$xk$6h$3u!2mh7ya8@nyshs4^v"
+SECRET_KEY = "django-insecure--0++i+z5+rho5n)zr*_6b#=+54&xs!7&yu5$0v6!9e%)y@9&jc"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
